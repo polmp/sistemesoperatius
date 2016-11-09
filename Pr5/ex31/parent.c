8faaa46d7873431b6d42ef402aa29ade5206d5d3
Pol MP <pol.moreno.puig@estudiant.upc.edu>
2016-11-09 18:33:30 +0100

    Editat 3.1

--- Pr5/ex31/parent.c
+++ Pr5/ex31/parent.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 
 int main(void){
+  pid_t llistapid[4];
   pid_t proces;
   int i=0;
   while(i++<4){
@@ -13,8 +14,9 @@ int main(void){
       exit(-1);
     }
 
-    if(proces!=0){ //Soc el pare
+    else if(proces!=0){ //Soc el pare
       //printf("Soc el papi amb PID %d i el meu fill es %d\n",getpid(),proces);
+      llistapid[i] = proces; //Guardem el valor a la llista de PIDS
       wait(NULL);
     }
     else{ //Soc el FILL
@@ -22,5 +24,6 @@ int main(void){
       break;
     }
   }
+
   return 0;
 }
