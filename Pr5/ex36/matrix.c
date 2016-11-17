340e43154e94fefa06154511433109be38d02631
Pol <pol.moreno.puig@estudiant.upc.edu>
2016-11-17 11:25:04 +0100

    Falta comprovar funcionament prod_matrix

--- Pr5/ex36/matrix.c
+++ Pr5/ex36/matrix.c
@@ -31,7 +31,8 @@ void prod_matrix(const matrix a, const matrix b, matrix r, int minf, int maxf, i
 	for(int i=minf-1;i<maxf;i++){
 		for(int j=minc-1;j<maxc;j++){
 			for(int index=0;index<DIM-1;index++){
-				r[i][j]+=a[i][j]*b[i][j];
+				printf("%d\n",index-1+minc);
+				r[i][j]+=a[i][index-1+minc]*b[index-1+minf][j];
 				
 			}
 		
@@ -49,7 +50,12 @@ float mr[DIM][DIM];
 const_matrix(m1,5);
 const_matrix(m2,3);
 const_matrix(mr,0);
+m1[0][1]=7;
+m1[0][2]=8;
+m2[0][2]=2;
 prod_matrix(m1,m2,mr,1,2,2,3);
+print_matrix(m1);
+print_matrix(m2);
 print_matrix(mr);
 return 0;
 }
