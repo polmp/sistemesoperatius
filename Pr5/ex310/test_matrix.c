67734fb5efbd432813f3ac4912d0da0f574fd1a2
Pol <pol.moreno.puig@estudiant.upc.edu>
2016-11-24 08:30:51 +0100

    ...

--- Pr5/ex310/test_matrix.c
+++ Pr5/ex310/test_matrix.c
@@ -5,8 +5,9 @@ int main(void){
 	float m1[DIM][DIM];
 	float m2[DIM][DIM];
 	float mr[DIM][DIM];
-	load_matrix("ola.dat",m1);
+	load_matrix("matA.dat",m1);
 	print_matrix(m1);
+	return 0;
 	/*
 	const_matrix(m2,3);
 	const_matrix(mr,0);
